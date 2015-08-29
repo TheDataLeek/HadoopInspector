a589f0c4b51ee0088b2331c1e0f475a02d2a331c
William Farmer <willzfarmer@gmail.com>
2015-08-29 11:25:02 -0600

    Removed venv directory

--- venv/bin/hadoopinspector_demogen.py
+++
@@ -1,4 +0,0 @@
-#!/home/william/PycharmProjects/impala-qa/venv/bin/python2
-# EASY-INSTALL-SCRIPT: 'hadoop-inspector==0.0.1','hadoopinspector_demogen.py'
-__requires__ = 'hadoop-inspector==0.0.1'
-__import__('pkg_resources').run_script('hadoop-inspector==0.0.1', 'hadoopinspector_demogen.py')
