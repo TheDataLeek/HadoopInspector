de3224b7f929a2bee339193bb93f59f88db186f7
William Farmer <willzfarmer@gmail.com>
2015-09-30 07:13:05 -0600

    Removed latin placeholder

--- server/hadinsp_httpserver.py
+++ server/hadinsp_httpserver.py
@@ -411,7 +411,8 @@ def checkdetails(instance, database, table, check):
     history['month'] = history['month'][-32:]
     history['week'] = history['week'][-8:]
 
-    desc = 'Proin suscipit luctus orci placerat fringilla. Donec hendrerit laoreet risus eget adipiscing. Suspendisse in urna ligula, a volutpat mauris. Sed enim mi, bibendum eu pulvinar vel, sodales vitae dui. Pellentesque sed sapien lorem, at lacinia urna. In hac habitasse platea dictumst. Vivamus vel justo in leo laoreet ullamcorper non vitae lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum ullamcorper rutrum.'
+    #desc = 'Proin suscipit luctus orci placerat fringilla. Donec hendrerit laoreet risus eget adipiscing. Suspendisse in urna ligula, a volutpat mauris. Sed enim mi, bibendum eu pulvinar vel, sodales vitae dui. Pellentesque sed sapien lorem, at lacinia urna. In hac habitasse platea dictumst. Vivamus vel justo in leo laoreet ullamcorper non vitae lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum ullamcorper rutrum.'
+    desc = ''
 
     content = render_template('checkdetails.html',
                         instance=instance,
